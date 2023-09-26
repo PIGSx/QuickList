91aef40496c024d39701c89f268f85d9db719fda
pigs <hiury1315ajac@gmail.com>
2023-09-25 22:53:18 -0300

    criacao  do menu

---
+++ menu.php
@@ -0,0 +1,38 @@
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>menu</title>
+    <!-- font -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;600;700;800&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.0/css/line.css">
+    <!-- css -->
+    <link rel="stylesheet" href="css/menu.css">
+</head>
+
+<body>
+    <div class="menu">
+        <nav>
+            <div class="container">
+                <input type="checkbox" id="check">
+                <a href="">
+                    <h3>name list</h3>
+                </a>
+                <ul class="links">
+                    <li><a href="#">exemple</a></li>
+                </ul>
+                <div class="hamburguer-menu-container">
+                    <div class="hamburguer-menu">
+                        <div></div>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    </div>
+</body>
+<script src="js/menu.js"></script>
+
+</html>
